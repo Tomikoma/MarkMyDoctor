66984f68202437ad8e3d93cea83a41fd1d4b6a68
Tamás Szabó <h775260@stud.u-szeged.hu>
2019-05-27 15:44:10 +0200

    Alert added after trying to add doctor

--- MarkMyDoctor/AddDoctorViewController.swift
+++ MarkMyDoctor/AddDoctorViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+struct MyMessage: Decodable {
+    let message:String
+}
+
 class AddDoctorViewController: UIViewController {
 
     @IBOutlet weak var nameTF: UITextField!
@@ -21,9 +25,13 @@ class AddDoctorViewController: UIViewController {
     }
 
     @IBAction func addDoctor(_ sender: UIButton) {
+        var msg:String?
+        var title:String?
         if let name = nameTF.text, let org = orgTF.text {
             if(name == "" || org == "" ){
-                makeAlert(msg: "Mindkét mezőt ki kell tölteni!")
+                //makeAlert(title:"Hiba", msg: "Mindkét mezőt ki kell tölteni!")
+                title="Hiba"
+                msg="Mindkét mezőt ki kell tölteni!"
             } else {
                 let parameters: [String:Any] = ["name" : name, "org" : org]
                 guard let url = URL(string: "http://localhost:3000/api/doctor") else { return }
@@ -31,28 +39,35 @@ class AddDoctorViewController: UIViewController {
                 request.httpMethod = "POST"
                 request.addValue("application/json", forHTTPHeaderField: "Content-Type")
                 guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters , options: [] ) else {
-                    makeAlert(msg: "Hiba történt a doktor hozzáadása közben!")
+                    makeAlert(title:"Hiba", msg: "Hiba történt a doktor hozzáadása közben!")
                     return
                 }
                 request.httpBody = httpBody
                 
                 let session = URLSession.shared
+                let sem = DispatchSemaphore.init(value: 0)
                 session.dataTask(with: request) { (data,response,error) in
                     if let data = data {
                         do {
-                            let json = try JSONSerialization.jsonObject(with: data, options: [] )
-                            print(json)
+                            let json = try JSONDecoder().decode(MyMessage.self, from: data)
+                            title = "Információ"
+                            msg = json.message
                         } catch {
                             print(error)
                         }
                     }
+                    sem.signal()
                 }.resume()
+                sem.wait()
             }
+        }
+        if let title = title, let msg = msg {
+            makeAlert(title: title, msg: msg)
         }
     }
     
-    func makeAlert(msg: String) {
-        let alert = UIAlertController(title: "Hiba", message: msg, preferredStyle: UIAlertController.Style.alert)
+    func makeAlert(title:String,msg: String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: UIAlertController.Style.alert)
         
         // add an action (button)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
